a3aedfc4361fc68d9b1a8f0864d548bc7023343b
SanketG <sanketgawande.gcoey@gmail.com>
2022-03-25 08:37:40 +0530

    debug msg renoved

--- _conn.php
+++ _conn.php
@@ -1,8 +1,8 @@
 <?php
-$host =  $_ENV['DB_HOST'];
-$user =  $_ENV['DB_USER'];
-$pass =  $_ENV['DB_PASS'];
-$database=$_ENV['DB_NAME'];
+$host =  "sql6.freesqldatabase.com"; // $_ENV['DB_HOST'];
+$user = "sql6481415";// $_ENV['DB_USER'];
+$pass = "Mj1QwyhhKH";// $_ENV['DB_PASS'];
+$database="sql6481415";//$_ENV['DB_NAME'];
 
 $link = mysqli_connect($host , $user , $pass , $database);
 
