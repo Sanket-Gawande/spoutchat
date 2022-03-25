a2452716a1cb0485ef8f9c154ff392941719884c
SanketG <sanketgawande.gcoey@gmail.com>
2022-03-25 08:19:38 +0530

    added env

--- _conn.php
+++ _conn.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost"; //. $_ENV['DB_HOST'];
-$user = "root" ; //$_ENV['DB_USER'];
-$pass = "";// $_ENV['DB_PASS'];
-$database ="chat_room" ;//$_ENV['DB_NAME'];
+$host =  $_ENV['DB_HOST'];
+$user =  $_ENV['DB_USER'];
+$pass =  $_ENV['DB_PASS'];
+$database=$_ENV['DB_NAME'];
 
 $link = mysqli_connect($host , $user , $pass , $database);
 
